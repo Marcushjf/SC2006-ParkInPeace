5e33d50dced99ce1eadc9125453dded0645f094b
Marcushjf <marcushjf@gmail.com>
2024-02-05 14:59:08 +0800

    Added files

---
+++ server/src/commonMain/kotlin/ntu26/ss/parkinpeace/server/controllers/SubscriptionController.kt
@@ -0,0 +1,7 @@
+package ntu26.ss.parkinpeace.server.controllers
+
+class SubscriptionController {
+    fun subscribe(): Unit = TODO()
+    fun unsubscribe(): Unit = TODO()
+    fun notify(a: Unit): Unit = TODO()
+}
